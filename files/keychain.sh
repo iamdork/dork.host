8910b7086b812bcee84fdbc0152e01ff64959b7b
pmelab <philipp.melab@gmail.com>
2015-05-16 15:03:38 +0200

    Fixed keychain call.

--- files/keychain.sh
+++ files/keychain.sh
@@ -1,5 +1,2 @@
-if [[ -z "$SSH_AUTH_SOCK" ]]
-then
-    keychain --noask -q $HOME/.ssh/id_rsa
-    source $HOME/.keychain/$HOSTNAME-sh
-fi
+keychain $HOME/.ssh/id_rsa
+source $HOME/.keychain/$HOSTNAME-sh
