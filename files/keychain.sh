c089f4a7301ba52bcc0b22742ba6b44f643fad3b
pmelab <philipp.melab@gmail.com>
2015-05-20 16:37:17 +0200

    Only add ssh-agent if SSH_AUTH_SOCK is undefined and there actually is a id_rsa for this user.

--- files/keychain.sh
+++ files/keychain.sh
@@ -1,2 +1,5 @@
-keychain $HOME/.ssh/id_rsa
-source $HOME/.keychain/$HOSTNAME-sh
+if [[ -z "$SSH_AUTH_SOCK" ]] && [[ -f $HOME/.ssh/id_rsa ]]
+then
+    keychain $HOME/.ssh/id_rsa
+    source $HOME/.keychain/$HOSTNAME-sh
+fi
